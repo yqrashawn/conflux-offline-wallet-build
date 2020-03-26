e509602e1f613f063bd5d697e3667b708a50afcd
yqrashawn <namy.19@gmail.com>
2020-03-26 19:18:12 +0800

    Fix: height

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ getPort().then(PORT => {
   ap.use(express.static(path.resolve(__dirname, "./conflux-offline-wallet")));
   const  server = ap.listen(PORT);
 
-
   app.on("ready", function () {
-    const mainWindow = new BrowserWindow({ show: true, width: 800, height: 700 });
+    const mainWindow = new BrowserWindow({ show: true, width: 800, height: 800 });
     mainWindow.loadURL(`http://localhost:${ PORT }`);
   });
 })
